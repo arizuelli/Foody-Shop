46a43d46fb6af573c9e7a28cf2add4c029eee253
arizuelli <112932375+arizuelli@users.noreply.github.com>
2022-09-08 17:40:34 +0200

    Update ProductEditScreen.js

--- ECOMMERCE/dashboard/src/screens/ProductEditScreen.js
+++ ECOMMERCE/dashboard/src/screens/ProductEditScreen.js
@@ -3,6 +3,7 @@ import Sidebar from "./../components/sidebar";
 import Header from "./../components/Header";
 import EditProductMain from "./../components/products/EditproductMain";
 
+//Pagina di aggiornamento prodotto. Richiama oltre a sidebar e header la pagina EditproductMain che aggiorna i dettagli del prodotto. Gli passa l'id del prodotto corrente
 const ProductEditScreen = ({ match }) => {
   const productId = match.params.id;
   return (
