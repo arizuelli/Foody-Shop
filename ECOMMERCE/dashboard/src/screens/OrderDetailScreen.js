6622d6abe13276d17bff189e00c8b6dbd91a2551
arizuelli <112932375+arizuelli@users.noreply.github.com>
2022-09-08 17:36:20 +0200

    Update OrderDetailScreen.js

--- ECOMMERCE/dashboard/src/screens/OrderDetailScreen.js
+++ ECOMMERCE/dashboard/src/screens/OrderDetailScreen.js
@@ -3,6 +3,7 @@ import Sidebar from "./../components/sidebar";
 import Header from "./../components/Header";
 import OrderDetailmain from "../components/orders/OrderDetailmain";
 
+//Pagina di analisi dettaglio del prodotto. Richiama (oltre a sidebar e Header come tutte le pagine nella cartella screen) OrderDetailMain che fa visualizzare i dettagli dei prodotti
 const OrderDetailScreen = ({ match }) => {
   const orderId = match.params.id;
   return (
