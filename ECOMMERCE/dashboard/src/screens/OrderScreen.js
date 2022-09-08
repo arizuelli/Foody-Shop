7790337b1e116ac914b69727c65a8a71dab3c61f
arizuelli <112932375+arizuelli@users.noreply.github.com>
2022-09-08 17:39:32 +0200

    Update OrderScreen.js

--- ECOMMERCE/dashboard/src/screens/OrderScreen.js
+++ ECOMMERCE/dashboard/src/screens/OrderScreen.js
@@ -3,6 +3,7 @@ import Sidebar from "./../components/sidebar";
 import Header from "./../components/Header";
 import OrderMain from "../components/orders/OrderMain";
 
+//Pagina principale degli ordini nel pannello Admin. Richiama come sempre sidebar, header e OrderMain che mostra gli ordini che sono stati effettuati 
 const OrderScreen = () => {
   return (
     <>
