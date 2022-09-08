ca82f17ee8014fc0cb5a06b20d6d397c311d2904
arizuelli <112932375+arizuelli@users.noreply.github.com>
2022-09-08 17:12:08 +0200

    Update HomeScreen.js

--- ECOMMERCE/dashboard/src/screens/HomeScreen.js
+++ ECOMMERCE/dashboard/src/screens/HomeScreen.js
@@ -3,6 +3,7 @@ import Header from "../components/Header";
 import Main from "../components/Home/Main";
 import Sidebar from "./../components/sidebar";
 
+//Pagina principale. La raggiungi appena accedi al pannello. Richiamo la sidebar a sinistra, l'header in alto e la pagina principale vera e propria contenuta nel main
 const HomeScreen = () => {
   return (
     <>
