fdb3994729728e5abd98727ef12c399b33b0bcd1
arizuelli <112932375+arizuelli@users.noreply.github.com>
2022-09-08 17:06:59 +0200

    Update index.js

--- ECOMMERCE/dashboard/src/index.js
+++ ECOMMERCE/dashboard/src/index.js
@@ -4,10 +4,13 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
 
+// The <App /> component is our root-level component. This means it’s at the very top of our component hierarchy.
+//The <Provider> component makes the Redux store available to any nested components that need to access the Redux store.
+
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <App /> 
     </React.StrictMode>
   </Provider>,
   document.getElementById("root")
