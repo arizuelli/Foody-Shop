d12582a9bf1eedc35e71c94c5cea2bec41730eeb
arizuelli <112932375+arizuelli@users.noreply.github.com>
2022-09-08 17:31:42 +0200

    Update AddProduct.js

--- ECOMMERCE/dashboard/src/screens/AddProduct.js
+++ ECOMMERCE/dashboard/src/screens/AddProduct.js
@@ -3,6 +3,7 @@ import Sidebar from "./../components/sidebar";
 import Header from "./../components/Header";
 import AddProductMain from "./../components/products/AddProductMain";
 
+//Pagina di aggiunta del prodotto. Richiama AddproductMain grazie alla quale avviene l'aggiunta vera e propria
 const AddProduct = () => {
   return (
     <>
